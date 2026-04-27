d6879392ab8630b9142b6014dcf677d062806e3b
Tushar Garg <gargtushar739@gmail.com>
2026-04-27 16:34:44 +0530

    Java LTS problem
    Signed-off-by: Tushar Garg <gargtushar739@gmail.com>

---
+++ src/main/java/com/adaptive/gateway/config/RateLimiterConfig.java
@@ -0,0 +1,17 @@
+package com.adaptive.gateway.config;
+
+import com.adaptive.gateway.service.RateLimiter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class RateLimiterConfig {
+    @Bean
+    public RateLimiter rateLimiter(
+        @Value("${ratelimiter.capacity:10}") int capacity,
+        @Value("${ratelimiter.refillRatePerSecond:1.0}") double refillRatePerSecond
+    ) {
+        return new RateLimiter(capacity, refillRatePerSecond);
+    }
+}
